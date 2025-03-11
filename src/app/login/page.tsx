bd0e54eda832a8ee2452d12c237e023714b669f6
amirbnsl <amir71084@gmail.com>
2025-03-11 02:25:29 +0100

    added login route

---
+++ src/app/login/page.tsx
@@ -0,0 +1,21 @@
+
+
+
+export default function Page() {
+    return (
+        <div>
+        <h1>Login</h1>
+        <form>
+            <div>
+            <label>Email</label>
+            <input type="email" />
+            </div>
+            <div>
+            <label>Password</label>
+            <input type="password" />
+            </div>
+            <button type="submit">Login</button>
+        </form>
+        </div>
+    );
+}
