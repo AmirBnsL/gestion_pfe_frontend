51aafcb7722d27cee7b5b7ad98a927b6ee67448e
amirbnsl <amir71084@gmail.com>
2025-05-27 01:45:48 +0100

    removed unused variable

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,11 +1,9 @@
 import type React from "react";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { AppSidebar } from "../components/app-sidebar/app-sidebar";
 import { AppNavbar } from "../components/app-navbar/page";
 import "../../app/globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Admin Dashboard",
